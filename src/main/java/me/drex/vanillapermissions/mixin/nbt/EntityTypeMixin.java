c211302d9f698fba64a6bea574f49aa94ae9ab10
Drex <nicknamedrex@gmail.com>
2025-03-23 10:58:18 +0100

    Update to 1.21.5-rc1

--- src/main/java/me/drex/vanillapermissions/mixin/nbt/EntityTypeMixin.java
+++ src/main/java/me/drex/vanillapermissions/mixin/nbt/EntityTypeMixin.java
@@ -1,12 +1,11 @@
 package me.drex.vanillapermissions.mixin.nbt;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import me.drex.vanillapermissions.util.Permission;
 import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -20,7 +19,7 @@ public abstract class EntityTypeMixin {
                     target = "Lnet/minecraft/server/players/PlayerList;isOp(Lcom/mojang/authlib/GameProfile;)Z"
             )
     )
-    private static boolean vanillaPermissions_addNbtLoadEntityPermission(boolean original, Level level, LivingEntity player) {
+    private static boolean vanillaPermissions_addNbtLoadEntityPermission(boolean original, @Local Player player) {
         return Permissions.check(player, Permission.NBT_LOAD_ENTITY, original);
     }
 }
