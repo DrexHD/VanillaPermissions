7b297bad426c35ef2553669639f8d32ab943c0aa
Drex <nicknamedrex@gmail.com>
2025-08-05 17:20:48 +0200

    Update to 1.21.8

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("dev.kikugie.stonecutter")
     id("org.jetbrains.changelog") version "2.2.0"
 }
-stonecutter active "1.21.7"
+stonecutter active "1.21.8"
 
 changelog {
     path = rootProject.file("CHANGELOG.md").path
