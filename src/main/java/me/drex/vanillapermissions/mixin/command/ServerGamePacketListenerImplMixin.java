13a103f94cd6f054fa2e4633f7a0c8c8e95593ba
qwerty吃小庄 <git@qwertycxz.top>
2026-05-09 21:14:39 +0800

    chore: Annoate unique private method

--- src/main/java/me/drex/vanillapermissions/mixin/command/ServerGamePacketListenerImplMixin.java
+++ src/main/java/me/drex/vanillapermissions/mixin/command/ServerGamePacketListenerImplMixin.java
@@ -24,6 +24,7 @@ import net.minecraft.world.level.gamerules.GameRules;
 //? }
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -129,6 +130,7 @@ public abstract class ServerGamePacketListenerImplMixin {
     }
     //? }
 
+    @Unique
     private boolean checkPermission(String command) {
         var parsed = player.level().getServer().getCommands().getDispatcher().parse(command, player.createCommandSourceStack());
         return parsed != null && getParseException(parsed) == null;
